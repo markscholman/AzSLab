29ae4169a7dd3a2d5fb62138454bc0fac964319e
Mark Scholman <mark.scholman@inovativ.nl>
2017-09-18 19:57:54 +0200

    Fix references for PE creation

--- Mgt Environment/WDS/BootImage/Create-WinPE.ps1
+++ Mgt Environment/WDS/BootImage/Create-WinPE.ps1
@@ -1,4 +1,4 @@
-﻿#run with the Deployment and Imaging Tools Environment (cmdlet)
+﻿#run with the Deployment and Imaging Tools Environment (cmdlet) - https://docs.microsoft.com/en-us/windows/deployment/windows-adk-scenarios-for-it-pros
 copype.cmd amd64 D:\WinPe_amd64
 Dism /mount-image /imagefile:D:\winpe_amd64\media\sources\boot.wim /index:1 /mountdir:D:\winpe_amd64\mount
 Dism /Image:D:\winpe_amd64\mount /Add-Package /PackagePath:"C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Windows Preinstallation Environment\amd64\WinPE_OCs\WinPE-WMI.cab"
@@ -7,7 +7,9 @@ Dism /Image:D:\winpe_amd64\mount /Add-Package /PackagePath:"C:\Program Files (x8
 Dism /Image:D:\winpe_amd64\mount /Add-Package /PackagePath:"C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Windows Preinstallation Environment\amd64\WinPE_OCs\WinPE-PowerShell.cab"
 Dism /Image:D:\winpe_amd64\mount /Add-Package /PackagePath:"C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Windows Preinstallation Environment\amd64\WinPE_OCs\WinPE-DismCmdlets.cab"
 Dism /Image:D:\winpe_amd64\mount /Add-Package /PackagePath:"C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Windows Preinstallation Environment\amd64\WinPE_OCs\WinPE-StorageWMI.cab"
+#Leave Deployment and Imaging Tools Environment (cmdlet) window open
 
+#In Powershell run
 $startnet = Get-Content D:\WinPe_amd64\mount\windows\system32\startnet.cmd
 $startnet += "`r`npowershell -c Set-ExecutionPolicy Unrestricted -Force"
 $startnet += "`r`npowershell -NoExit -c X:\DeployAzureStackPOC.ps1"
@@ -16,6 +18,7 @@ $startnet
 Set-Content -Value $startnet -Path "D:\WinPe_amd64\mount\windows\system32\startnet.cmd" -Force
 #Set-Content -Value "wpeinit" -Path "D:\WinPe_amd64\mount\windows\system32\startnet.cmd" -Force
 
+#Back in Deployment and Imaging Tools Environment (cmdlet) window
 Dism /unmount-image /mountdir:D:\winpe_amd64\mount /commit
 MakeWinPEMedia /ISO D:\WinPE_amd64 D:\WinPE_amd64\WinPE_amd64.iso
 
